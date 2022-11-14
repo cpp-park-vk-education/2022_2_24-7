cd8005bd72f9aaf11d8effcabb4e857a8f85d0a6
KolobokMysnoy <Inkasator200@ya.ru>
2022-11-14 12:02:21 +0000

    Global rename files and transport tests

--- cppLintCheck.py
+++ cppLintCheck.py
@@ -1,11 +1,13 @@
 import subprocess
 import os
-for root, dirs, files in os.walk("./l1"):
+for root, dirs, files in os.walk("."):
     for file in files:
-        if file.endswith(".cpp"):
-             subprocess.run(['cpplint', os.path.join(root, file)])
+        if not root.startswith("./build"):
+            if file.endswith(".cpp"):
+                subprocess.run(['cpplint', os.path.join(root, file)])
 
-for root, dirs, files in os.walk("./l1"):
+for root, dirs, files in os.walk("."):
     for file in files:
-        if file.endswith(".cpp"):
-             subprocess.run(['cppcheck', os.path.join(root, file)])
+        if not root.startswith("./build"):
+            if file.endswith(".cpp"):
+                subprocess.run(['cppcheck', os.path.join(root, file)])
--- tests/testExampleLib/tests.cpp
+++ tests/testExampleLib/tests.cpp
@@ -5,3 +5,7 @@
 TEST(DefaultTest, Test) {
     EXPECT_EQ((2 * 2), 4);
 }
+
+TEST(Test1, test2) {
+    EXPECT_EQ(3, ret_int(2));
+}
