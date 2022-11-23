1c5078ddf1fffce83ab8feafd6ea041c35545522
Gevork Gabrielyan <gevorkgabrielan@gmail.com>
2022-11-23 20:01:16 +0300

    format

--- format_check.sh
+++ format_check.sh
@@ -4,7 +4,7 @@ THIS_DIR="$(dirname "$THIS_PATH")"
 FILE_LIST1="$(find "$THIS_DIR/client_side" | grep -E ".*(\.cpp|\.h|\.hpp|\.hh)$")"
 FILE_LIST2="$(find "$THIS_DIR/server_side" | grep -E ".*(\.cpp|\.h|\.hpp|\.hh)$")"
 FILE_LIST3="$(find "$THIS_DIR/tests" | grep -E ".*(\.cpp|\.h|\.hpp|\.hh)$")"
-echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""
+echo -e "Files found to format = \n\"\"\"\n${FILE_LIST1}\n\"\"\"\n${FILE_LIST2}\n\"\"\"\n${FILE_LIST3}\n\"\"\""
 # Run clang-format
 clang-format --verbose -i --style=file ${FILE_LIST1}
 clang-format --verbose -i --style=file ${FILE_LIST2}
