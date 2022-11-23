f7afdfd4c4f5da21f7fbc20fd5d9f78f1508b113
ned8800 <tchichikin@mail.ru>
2022-11-24 01:58:30 +0300

    added cpplint script

--- format_check.sh
+++ format_check.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+append_newline () {
+    if [ -z "$(tail -c 1 "$1")" ]; then
+        :
+    else
+        echo >> "$1"
+    fi
+}
 THIS_PATH="$(realpath "$0")"
 THIS_DIR="$(dirname "$THIS_PATH")"
 FILE_LIST1="$(find "$THIS_DIR/client_side" | grep -E ".*(\.cpp|\.h|\.hpp|\.hh)$")"
@@ -9,3 +16,12 @@ echo -e "Files found to format = \n\"\"\"\n${FILE_LIST1}\n\"\"\"\n${FILE_LIST2}\
 clang-format --verbose -i --style=file ${FILE_LIST1}
 clang-format --verbose -i --style=file ${FILE_LIST2}
 clang-format --verbose -i --style=file ${FILE_LIST3}
+for f in ${FILE_LIST1}
+do append_newline $f 
+done
+for f in ${FILE_LIST2}
+do append_newline $f 
+done
+for f in ${FILE_LIST3}
+do append_newline $f 
+done
