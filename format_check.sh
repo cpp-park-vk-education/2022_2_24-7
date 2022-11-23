7c6fb574be16e971c2dfb1557466070e1ac617c5
KolobokMysnoy <Inkasator200@ya.ru>
2022-11-23 15:56:27 +0000

    CMAKE

--- format_check.sh
+++ format_check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 THIS_PATH="$(realpath "$0")"
 THIS_DIR="$(dirname "$THIS_PATH")"
 
@@ -9,3 +11,4 @@ echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""
 
 # Run clang-format
 clang-format --verbose -i --style=file ${FILE_LIST}
+
--- server_side/main.cpp
+++ server_side/main.cpp
@@ -1,1 +1,9 @@
 int main() { return 0; }
+
+
+
+
+
+
+
+
