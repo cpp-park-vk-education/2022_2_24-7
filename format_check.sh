a6f1905d19cbdfc50d0b6fa86bfcc8e976d3e1c0
ned8800 <tchichikin@mail.ru>
2022-11-23 18:20:49 +0300

    added clang-format script, settings

--- format_check.sh
+++ format_check.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+set -e
+
+append_newline () {
+    if [ -z "$(tail -c 1 "$1")" ]; then
+        :
+    else
+        echo >> "$1"
+    fi
+}
+
 THIS_PATH="$(realpath "$0")"
 THIS_DIR="$(dirname "$THIS_PATH")"
 
-FILE_LIST="$(find "$THIS_DIR" | grep -E ".*(\.cpp|\.h|\.hpp|\.hh)$")"
+FILE_LIST="$(find "$THIS_DIR/" | grep -E ".*(\.cpp|\.h|\.hpp|\.hh)$")"
 
 echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""
 
-clang-format --verbose -i --style=file $FILE_LIST
+# Run clang-format
+clang-format --verbose -i --style=file ${FILE_LIST}
+
+# Check newlines
+for f in ${FILE_LIST}; do
+    append_newline $f
+done
--- tests/testExampleLib/tests.cpp
+++ tests/testExampleLib/tests.cpp
@@ -1,10 +1,7 @@
 #include <gtest/gtest.h>
+
 #include "server.hpp"
 
-TEST(DefaultTest, Test) {
-    EXPECT_EQ((2 * 2), 4);
-}
+TEST(DefaultTest, Test) { EXPECT_EQ((2 * 2), 4); }
 
-TEST(DefaultTest2, Test) {
-    EXPECT_EQ((2 * 2), 4);
-}
+TEST(DefaultTest2, Test) { EXPECT_EQ((2 * 2), 4); }
