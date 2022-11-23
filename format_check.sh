b2081417caa30f615bcc465b462d893f634c09ab
ned8800 <tchichikin@mail.ru>
2022-11-23 19:13:03 +0300

    added clang-format script, settings 1.4

--- format_check.sh
+++ format_check.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-
 THIS_PATH="$(realpath "$0")"
 THIS_DIR="$(dirname "$THIS_PATH")"
-
 FILE_LIST="$(find "$THIS_DIR/" | grep -E ".*(\.cpp|\.h|\.hpp|\.hh)$")"
-
 echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""
-
 # Run clang-format
 clang-format --verbose -i --style=file ${FILE_LIST}
