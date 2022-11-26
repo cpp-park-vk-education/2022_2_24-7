00609c0a49def6540fdef3849ddfb08806399b51
ned8800 <tchichikin@mail.ru>
2022-11-26 20:06:10 +0300

    coverage script

--- scripts/coverage_check.sh
+++ scripts/coverage_check.sh
@@ -5,5 +5,6 @@ print_lint_header() {
 print_lint_header "COVERAGE"
 cmake -B build
 cmake --build build
-gcovr -s ./build --xml coverage.xml --filter ../server_side/ --filter ../client_side/
+print_lint_header "REPORT"
+gcovr -s ./build --filter ../server_side/ --filter ../client_side/ #--xml coverage.xml
 print_lint_header "SUCCESS"
